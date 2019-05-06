2af74f49713055e45beca54b72207f653f691d2a
Jackson <jacksonzamo@gmail.com>
2019-05-06 16:34:56 -0600

    Initial Commit

---
+++ Weave/Weave.h
@@ -0,0 +1,19 @@
+//
+//  Weave.h
+//  Weave
+//
+//  Created by Jackson Zamorano on 5/6/19.
+//  Copyright © 2019 Jackson Zamorano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Weave.
+FOUNDATION_EXPORT double WeaveVersionNumber;
+
+//! Project version string for Weave.
+FOUNDATION_EXPORT const unsigned char WeaveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Weave/PublicHeader.h>
+
+

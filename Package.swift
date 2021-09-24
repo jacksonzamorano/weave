bfb19a4ae4dbe158613ade493205ef5dec9e7897
Jackson Zamorano <jackson@jacksonzamorano.com>
2021-09-23 22:49:36 -0600

    Added platform versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Weave",
+    platforms: [
+        .iOS("12.0"), .macOS("10.13"), .watchOS("4.0")
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
